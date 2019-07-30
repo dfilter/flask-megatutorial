14f24611f794ac723dbe5a06b4875a338db80f07
dfilter <dfilter@evertz.com>
2019-07-30 15:05:13 -0400

    Chapter 2: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = dict(username='dfilter')
+    posts = [
+        dict(author=dict(username='John'), 
+             body='Beautiful day in Burlington!'),
+        dict(author=dict(username='Susan'), 
+             body='When is John comming back?!')]
+    return render_template('index.html', title='Home', user=user, posts=posts)
