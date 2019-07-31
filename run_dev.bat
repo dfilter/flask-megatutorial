a02130fce6f9fb516949d4fe3cac16da27179dd8
dfilter <dfilter@evertz.com>
2019-07-31 15:07:04 -0400

    Chapter 7: Error Handling

--- run_dev.bat
+++ run_dev.bat
@@ -1,1 +1,11 @@
+rem ---------------------------------------
+rem below are the parameters used for gmail server
+rem set MAIL_SERVER=smtp.googlemail.com
+rem set MAIL_PORT=587
+rem set MAIL_USE_TLS=1
+rem set MAIL_USERNAME=<your-gmail-username>
+rem set MAIL_PASSWORD=<your-gmail-password>
+rem ---------------------------------------
+set FLASK_ENV=development
+set FLASK_DEBUG=1
 python -m flask run
