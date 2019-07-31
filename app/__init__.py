1b69387d5b56239e1700633d194ac0ce7b4a2041
dfilter <dfilter@evertz.com>
2019-07-31 12:28:12 -0400

    Chapter 5: User Logins

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'  # note that this points to the login def not route
 
 from app import models, routes
