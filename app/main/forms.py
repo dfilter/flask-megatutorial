960476eed4810660ee8f07bd227809c406b896da
dfilter <dfilter@evertz.com>
2019-12-13 14:12:42 -0500

    added elasticsearch

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,6 +8,17 @@ from wtforms.validators import DataRequired, Length, ValidationError
 from app.models import User
 
 
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
+
+
 class EditProfileForm(FlaskForm):
     username = StringField(_l('Username'), validators=[DataRequired()])
     about_me = TextAreaField(_l('About me'),
