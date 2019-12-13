39a43f7120551bb27aa65da21bba82d6d601d305
dfilter <dfilter@evertz.com>
2019-12-13 16:37:40 -0500

    Chapter 21: User Notifications

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,6 +8,12 @@ from wtforms.validators import DataRequired, Length, ValidationError
 from app.models import User
 
 
+class MessageForm(FlaskForm):
+    message = TextAreaField(_l('Message'), validators=[
+        DataRequired(), Length(min=0, max=140)])
+    submit = SubmitField(_l('Submit'))
+
+
 class SearchForm(FlaskForm):
     q = StringField(_l('Search'), validators=[DataRequired()])
 
--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config(object):
 
     POSTS_PER_PAGE = 4
 
-    LANGUAGES = ['en-CA', 'fr-CA']
+    LANGUAGES = ['en', 'fr']
 
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
 
